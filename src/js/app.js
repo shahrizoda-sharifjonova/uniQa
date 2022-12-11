e5e184ca3928091edd0da4a93768cfa85a744ef3
shahrizoda <shahrizodadeveloping@gmail.com>
2022-12-11 14:52:35 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,10 @@ functions.isWebp();
 
 // const swiper = new Swiper();
 
+const navbarToggler = document.querySelector('.navbar-toggler');
+const body = document.querySelector('body');
+
+navbarToggler.addEventListener('click', (e)=>{
+    body.classList.toggle('hidden')
+})
+
